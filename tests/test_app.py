3cadd0c82b5f16230d03acc318b612d190618881
root <root@kali.kali>
2026-08-20 22:48:46 -0400

    Corregir pruebas y generar cobertura

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_login_page(client):
+    response = client.get('/login')
+    assert response.status_code == 200
+
+def test_register_page(client):
+    response = client.get('/register')
+    assert response.status_code == 200
